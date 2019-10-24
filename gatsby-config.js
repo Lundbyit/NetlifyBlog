43d7ab431dd8b10d0262ed45be28a86d18f3a3f4
Lundbyit <Lundbyit@users.noreply.github.com>
2019-10-24 21:41:31 +0200

    Changed social account

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: `Notes from a developer`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `lundbyit`,
     },
   },
   plugins: [
